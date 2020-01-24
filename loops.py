4a221e4b98cd9c4cb8a41fde0fb5665b80eacc24
Hannah <reillyhannah65@gmail.com>
2020-01-24 10:38:56 -0500

    Test

---
+++ loops.py
@@ -0,0 +1,17 @@
+# items = [100, 200, 300, 400, 500, 600, 700]
+# item_len = len(items)
+# print(item_len)
+
+# for i in items:
+#     print(i)
+
+# my_range = range(len(items))
+# print(type(my_range))
+#
+# for i in range(0, len(items), 2):
+#     print(items[i])
+
+items = [200, 300, 400, 500, 600, 700, 800, 900, 1000]
+
+for i in range(1, len(items)-1, 2):
+    print(items[i])
